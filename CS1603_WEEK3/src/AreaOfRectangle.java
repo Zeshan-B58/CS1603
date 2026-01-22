04164a2fd764a520f18f221ebbe29ec9b6190277
Zeshan <1844252@brunel.ac.uk>
2026-01-22 12:59:44 +0000

    Add files via upload

---
+++ CS1603_WEEK3/src/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+public class AreaOfRectangle {
+	
+    public static void main(String[] args) {
+        // Declare and assign variables for length and width
+        double l = 10.5;
+        int w = 5;
+        
+        // Expression to calculate the area
+        double A = l * w;
+        
+        // Print the result
+        System.out.println("Area of the rectangle: " + A);
+    }
+}
