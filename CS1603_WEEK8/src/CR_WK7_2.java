d6fce3fb90051cda1a4f017c09fd0200013bb898
Zeshan <1844252@brunel.ac.uk>
2026-01-22 13:00:42 +0000

    Add files via upload

---
+++ CS1603_WEEK8/src/CR_WK7_2.java
@@ -0,0 +1,24 @@
+
+public class CR_WK7_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//DoSomeCalcuatons=(5, 5, 1, 1);
+
+		MakeSequence(5);
+	}
+	
+		public static void DoSomeCalculation(double a, double b, double c, double d) 
+		{
+			System.out.println((a+b)/(c+d));
+		
+
+			//"111112222333445"
+		}
+		public static String MakeSequence(int N) 
+		{
+      for 
+			return "";
+		}
+}
---
+++ CS1603_WEEK8/src/CS_WK7_1.java
@@ -0,0 +1,19 @@
+
+public class CS_WK7_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DoSomeCalculation(5, 5, 1, 1);
+	}
+		
+	public static void DoSomeCalculation(double a, double b, double c, double d) 
+	{
+
+		System.out.println((a+b)/(c+d));
+	}
+}	
+
+	
+
+
