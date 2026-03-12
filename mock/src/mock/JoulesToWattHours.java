37e8ea4c43cdf97dd5fa80543ad857426642db12
Zeshan <1844252@brunel.ac.uk>
2026-03-12 13:04:11 +0000

    Add files via upload

---
+++ mock/src/mock/JoulesToWattHours.java
@@ -0,0 +1,20 @@
+package mock;
+
+public class JoulesToWattHours {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double j2w = JoulesToWattsHours(78.8);
+		System.out.println(j2w);
+	}
+	
+	public static double JoulesToWattsHours (double j)
+	{
+		if (j<= 0.0) return(-1);
+		return(90.0);
+		
+		
+	}
+
+}
---
+++ mock/src/mock/Quandrant.java
@@ -0,0 +1,14 @@
+package mock;
+
+public class Quandrant {
+
+	public Quandrant() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
